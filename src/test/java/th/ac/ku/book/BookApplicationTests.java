17e28534e6fa4be62bf78a24a885d2b7699dd59b
Zahil Chuduang <zahil.c@ku.th>
2022-09-13 02:38:34 +0700

    add AddBookTests test unit

--- src/test/java/th/ac/ku/book/BookApplicationTests.java
+++ src/test/java/th/ac/ku/book/BookApplicationTests.java
@@ -1,6 +1,8 @@
 package th.ac.ku.book;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +11,4 @@ class BookApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
